564792448581593a4b7cb91bf713b8f9f5431c40
IsharaU <wellahewawiud.20@uom.lk>
2024-10-10 11:59:45 +0530

    delete unwanted files

--- Canny_Edge_Detection.py
+++ Canny_Edge_Detection.py
@@ -4,38 +4,6 @@ import sys
 import numpy as np
 from PIL import Image
 
-# # see jpg,png or bmp files in the directory
-# valid_extensions = [".jpg", ".png", ".bmp"]
-
-# # list all files in the current directory
-# current_directory = os.getcwd()
-# files = [f for f in os.listdir(current_directory) if os.path.isfile(os.path.join(current_directory, f))]
-
-# # filter out the files with valid extensions
-# valid_files = [f for f in files if any(f.endswith(ext) for ext in valid_extensions)]
-
-# number = 1
-# # choose a file to process
-
-# print("Available images:")
-# for image_name in valid_files:
-#     print(f"{number}. {image_name}")
-#     number += 1
-
-# while True:
-#     try:
-#         input_number = int(input("Enter the number of the image to process: "))
-#         chosen_image = valid_files[input_number - 1]
-#         break
-#     except ValueError:
-#         print("Invalid input. Please enter a number.")
-#         continue
-#     except IndexError:
-#         print("Invalid input. Please enter a number within the range.")
-#         continue
-
-# print(f"You chose {chosen_image}")
-
 # convert to grayscale 
 def convert_to_grayscale(image_path):
     img = Image.open(image_path)
